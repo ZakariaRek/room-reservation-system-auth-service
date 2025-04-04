a5541b7a3d8361c545e2c9f1c9e1ea1b24b676fd
ZakariaRekhla2004 <zakariaest49@gmail.com>
2025-04-04 23:05:20 +0000

    update CI_CD pipeline

--- src/test/java/com/reservation_system/authService/SpringBootSecurityJwtApplicationTests.java
+++ src/test/java/com/reservation_system/authService/SpringBootSecurityJwtApplicationTests.java
@@ -2,8 +2,22 @@ package com.reservation_system.authService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test") // This will load application-test.properties/yml if available
+@TestPropertySource(properties = {
+		// Override any critical properties that might be causing issues
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.driverClassName=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=password",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"jwt.secret=testsecretkey",
+		"jwt.expirationMs=86400000"
+})
 public class SpringBootSecurityJwtApplicationTests {
 	@Test
 	public void contextLoads() {
